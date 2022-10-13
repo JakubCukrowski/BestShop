938826ca715be1d87e4e08e06a8613c7cb756e12
JakubCukrowski <j.d.cukrowski@gmail.com>
2022-10-13 20:41:46 +0200

    Calculator works

--- dist/index.3b5d40e2.js
+++ dist/index.3b5d40e2.js
@@ -1,47 +1,105 @@
-// ELEMENTS WITH EVENT
 const productsQuantity = document.querySelector("#products");
 const ordersPerMonth = document.querySelector("#orders");
 const selectPackage = document.querySelector("#package");
-const dropdownMenu = document.querySelector(".select__input, :after");
+const dropdownInput = document.querySelector(".select__input");
+const dropdownMenu = document.querySelector(".select__dropdown");
 const accountingPrice = document.querySelector("#accounting");
 const terminalPrice = document.querySelector("#terminal");
-// HIDDEN ELEMENTS
-const productsData = document.querySelector('[data-id = "products"]');
-const productsCalculate = document.querySelector('[data-id = "products"]').children[1];
-const productsPrice = document.querySelector('[data-id = "products"]').children[2];
-const ordersData = document.querySelector('[data-id = "orders"]');
-const ordersCalculate = document.querySelector('[data-id = "orders"]').children[1];
-const ordersPrice = document.querySelector('[data-id = "orders"]').children[2];
-const packageData = document.querySelector('[data-id = "package"]');
-const packageCalculate = document.querySelector('[data-id = "package"]').children[1];
-const packagePrice = document.querySelector('[data-id = "package"]').children[2];
+const packages = document.querySelector(`[data-id="package"]`);
+const listItems = document.querySelectorAll(".list__item");
 const totalPrice = document.querySelector(".total__price");
+const accountingPriceFromCheckBox = document.querySelector(`[data-id="accounting"] .item__price`);
+const terminalPriceFromCheckBox = document.querySelector(`[data-id="terminal"] .item__price`);
+accountingPriceFromCheckBox.value = 0;
+packages.children[2].value = 0;
 totalPrice.textContent = "$0";
-productsQuantity.addEventListener("input", function() {
-    if (productsQuantity.value.length > 0) {
-        packageData.classList.add("open");
-        totalPrice.classList.add("open");
-        packageCalculate.textContent = productsQuantity.value + " * " + 0.5;
-        packagePrice.textContent = "$" + productsQuantity.value * 0.5;
-        updateTotal();
-    } else if (productsQuantity.value.length === 0) packageData.classList.remove("open");
-});
-ordersPerMonth.addEventListener("input", function() {
-    if (ordersPerMonth.value.length > 0) {
-        ordersData.classList.add("open");
-        totalPrice.classList.add("open");
-        ordersCalculate.textContent = ordersPerMonth.value + " * " + 0.5;
-        ordersPrice.textContent = "$" + ordersPerMonth.value * 0.5;
-        updateTotal();
-    } else if (ordersPerMonth.value.length === 0) ordersData.classList.remove("open");
-});
-dropdownMenu.addEventListener("click", function() {
-    dropdownMenu.parentElement.classList.toggle("open");
-});
-function updateTotal() {
-    const productsSum = productsQuantity.value * 0.5;
-    const ordersSum = ordersPerMonth.value * 0.5;
-    totalPrice.textContent = "$" + (productsSum + ordersSum);
-}
+terminalPriceFromCheckBox.value = 0;
+const inputPrice = (e)=>{
+    for (const item of listItems)if (item.dataset.id === e.target.id) {
+        if (e.currentTarget.value.length > 0) {
+            item.classList.add("open");
+            item.children[1].textContent = e.target.value + "*" + 0.5;
+            item.children[2].textContent = "$" + e.target.value * 0.5;
+        } else if (e.currentTarget.value.length === 0) {
+            item.classList.remove("open");
+            item.children[2].textContent = "$0";
+        }
+    }
+    summaryTotal();
+};
+const togglePackages = ()=>{
+    selectPackage.classList.toggle("open");
+};
+const chooseFromDrop = (e)=>{
+    if (e.target.dataset.value === "basic") {
+        packages.classList.add("open");
+        packages.children[1].textContent = "Basic";
+        packages.children[2].textContent = "$10";
+        packages.children[2].value = 10;
+        selectPackage.classList.remove("open");
+        dropdownInput.textContent = packages.children[1].textContent;
+    } else if (e.target.dataset.value === "professional") {
+        packages.classList.add("open");
+        packages.children[1].textContent = "Professional";
+        packages.children[2].textContent = "$25";
+        packages.children[2].value = 25;
+        selectPackage.classList.remove("open");
+        dropdownInput.textContent = packages.children[1].textContent;
+    } else if (e.target.dataset.value === "premium") {
+        packages.classList.add("open");
+        packages.children[1].textContent = "Premium";
+        packages.children[2].textContent = "$60";
+        packages.children[2].value = 60;
+        selectPackage.classList.remove("open");
+        dropdownInput.textContent = packages.children[1].textContent;
+    }
+    summaryTotal();
+};
+const handleAccounting = ()=>{
+    if (accountingPrice.checked === true) {
+        for (const listItem of listItems)if (listItem.dataset.id === "accounting") {
+            listItem.classList.add("open");
+            accountingPriceFromCheckBox.value = 30;
+            accountingPriceFromCheckBox.textContent = "$" + accountingPriceFromCheckBox.value;
+        }
+    } else {
+        for (const listItem1 of listItems)if (listItem1.dataset.id === "accounting") {
+            listItem1.classList.remove("open");
+            accountingPriceFromCheckBox.value = 0;
+            accountingPriceFromCheckBox.textContent = "$" + accountingPriceFromCheckBox.value;
+        }
+    }
+    summaryTotal();
+};
+const handleTerminal = ()=>{
+    if (terminalPrice.checked === true) {
+        for (const listItem of listItems)if (listItem.dataset.id === "terminal") {
+            listItem.classList.add("open");
+            terminalPriceFromCheckBox.value = 15;
+            terminalPriceFromCheckBox.textContent = "$" + terminalPriceFromCheckBox.value;
+        }
+    } else {
+        for (const listItem1 of listItems)if (listItem1.dataset.id === "terminal") {
+            listItem1.classList.remove("open");
+            terminalPriceFromCheckBox.value = 0;
+            terminalPriceFromCheckBox.textContent = "$" + terminalPriceFromCheckBox.value;
+        }
+    }
+    summaryTotal();
+};
+const summaryTotal = ()=>{
+    const productsPrice = productsQuantity.value;
+    const ordersPrice = ordersPerMonth.value;
+    const packagePrice = packages.children[2].value;
+    const accountingPrice = accountingPriceFromCheckBox.value;
+    const terminalPrice = terminalPriceFromCheckBox.value;
+    totalPrice.textContent = "$" + ((+productsPrice + +ordersPrice) * 0.5 + +packagePrice + +accountingPrice + +terminalPrice);
+};
+productsQuantity.addEventListener("input", inputPrice);
+ordersPerMonth.addEventListener("input", inputPrice);
+dropdownInput.addEventListener("click", togglePackages);
+dropdownMenu.addEventListener("click", chooseFromDrop);
+accountingPrice.addEventListener("change", handleAccounting);
+terminalPrice.addEventListener("change", handleTerminal);
 
 //# sourceMappingURL=index.3b5d40e2.js.map
